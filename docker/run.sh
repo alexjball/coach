d6bb0a01454b3bf87edf592089a83fb02a0044be
Alex Ball <ball.alex.509@gmail.com>
2020-02-10 17:11:29 -0500

    Make Coach Dockerfiles usable.
    - Tweak dep versions so that `coach -l` succeeds
    - Add a run file with up, down, and shell commands

---
+++ docker/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+
+ROOT=$(realpath $(dirname $BASH_SOURCE)/..)
+
+cd $ROOT
+
+run() {
+  case "$1" in
+    build)
+      build-images
+      ;;
+    up)
+      docker run \
+          -d \
+          --rm \
+          --net host \
+          -v /tmp/checkpoint:/checkpoint \
+          --name coach \
+          coach:master \
+          tail -f /dev/null
+      ;;
+    down)
+      docker stop coach
+      ;;
+    shell)
+      docker exec -it coach /bin/bash 
+      ;;
+  esac
+}
+
+build-images() {
+  docker build -f docker/Dockerfile.base -t coach-base:master .
+  docker build -f docker/Dockerfile.gym_environment -t coach:master .
+}
+
+run $@
